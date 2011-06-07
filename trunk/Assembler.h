0ed9b68fc8e1da12dcc32e3f705c21137773ca32
timothy.kelsall <timothy.kelsall@df0bca5f-0050-d295-148b-4fc54db3a585>
2011-06-07 13:14:05 +0000

    Added opcodes for mnemonics, expanded on the Assembler class definition.

--- trunk/Assembler.h
+++ trunk/Assembler.h
@@ -10,22 +10,10 @@
 #define ASSEMBLER_H
 
 #include <map>
+#include <vector>
 #include <fstream>
 
-// Helper structs
-struct label_struct {
-	std::string* label;
-	int line;
-	int address;
-	bool hasAddress;
-
-	label_struct* next;
-};
-
-struct labels {
-	label_struct* head;
-	int count;
-};
+#include "Opcodes.h"
 
 // Assembler class, does the hard work
 class Assembler {
@@ -39,10 +27,28 @@ public:
 	void outputFile();
 
 private:
+	// Remove ',' '\t' etc.
+	void cleanLine(const std::string&);
+	// Import another asm file (INCLUDE)
+	bool importInc(const std::string&);
+	// Import a binary file (IMPORTBIN)
+	bool importBin(const std::string&, int, int, const std::string&);
+
+	void op_void(OPCODE);			// nop, cls, vblnk, ret, snd0, pushall, popall
+	void op_imm(OPCODE);			// jmp, jmc, jmz, jx, call, cx, spr, snd[1-3],
+	void op_n(OPCODE);				// bgc
+	void op_n_n(OPCODE);			// flip
+	void op_r_imm(OPCODE);			// rnd, ldi, ldm, stm, addi, subi, muli, divi, cmpi, andi, tsti, ori, xori
+	void op_r_n(OPCODE);			// shl, shr, sal, sar
+	void op_r_r_imm(OPCODE);		// drw, jme
+	void op_r_r_r(OPCODE);			// add, sub, mul, div, cmp, and, tst, or, xor
+	void db(std::vector<unsigned char>&);
+
 	std::map<char*,int> labels;
 	std::map<char*,int> consts;
-
-	std::ifstream input;
+	// Source code buffer
+	std::string input;
+	// Output file stream
 	std::ofstream output;
 	
 	int lineNb;
---
+++ trunk/Opcodes.h
@@ -0,0 +1,30 @@
+/*    
+	tchip16, an open-source Chip16 assembler
+    Copyright (C) 2011  Tim Kelsall
+	[...]
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPCODES_H
+#define OPCODES_H
+
+typedef OPCODE	unsigned char;
+
+enum chip16_opcodes {
+	NOP =	0x00, CLS, VBLNK, BGC, SPR, DRW_I, DRW_R, RND, FLIP, SND0, SND1, SND2, SND3,
+	JMP_I = 0x10, JMC, Jx, JME, CALL_I, RET, JMP_R, Cx, CALL_R,
+	LDI_R = 0x20, LDI_SP, LDM_I, LDM_R, MOV,
+	STM_I = 0x30, STM_R,
+	ADDI =	0x40, ADD_R2, ADD_R3,
+	SUBI =	0x50, SUB_R2, SUB_R3, CMPI, CMP,
+	ANDI =	0x60, AND_R2, AND_R3, TSTI, TST,
+	ORI =	0x70, OR_R2, OR_R3,
+	XORI =	0x80, XOR_R2, XOR_R3,
+	MULI =	0x90, MUL_R2, MUL_R3,
+	DIVI =	0xA0, DIV_R2, DIV_R3,
+	SHL_N = 0xB0, SAL_N = 0xB0, SHR_N, SAR_N, SHL_R, SAL_R=0xB3, SHR_R, SAR_R,
+	PUSH =	0xC0, POP, PUSHALL, POPALL
+};
+
+#endif

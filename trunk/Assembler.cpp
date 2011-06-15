f8f97180a852588e829b4fd09b682ffe8cb972fb
timothy.kelsall <timothy.kelsall@df0bca5f-0050-d295-148b-4fc54db3a585>
2011-06-15 23:56:48 +0000

    Functional (but incomplete) tokenizing loop; added the file-open method call in main. More messing around with how the Assembler works.

--- trunk/Assembler.cpp
+++ trunk/Assembler.cpp
@@ -6,10 +6,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <iostream>
+
 #include "Assembler.h"
 
 Assembler::Assembler() {
-	input.reserve(20*1024*1024);
 	buffer = new u8[MEM_SIZE]();
 	lineNb = 0;
 	curAddress = 0;
@@ -25,7 +26,16 @@ Assembler::~Assembler() {
 }
 
 bool Assembler::openFile(const char* fn) {
-	
+	std::ifstream in(fn);
+	if(in.is_open()) {
+		std::stringstream file;
+		file << in.rdbuf();
+		/*std::string file((std::istreambuf_iterator<char>(in)), 
+						  std::istreambuf_iterator<char>());*/
+		input.push(file.str());
+		in.close();
+		return true;
+	}
 	return false;
 }
 
@@ -33,9 +43,42 @@ void Assembler::setOutputFile(const char* fn) {
 	outputFP = fn;
 }
 
-bool Assembler::importInc(const std::string& fn) {
-	
-	return false;
+void Assembler::tokenize() {
+	// Get a line
+	std::string ln;
+	// This only works for single file processing
+	// Need to figure out how to get multiple files working
+	std::stringstream s(input.top());
+	while(std::getline(s,ln)) {
+		// Get tokens from the line
+		line toks;
+		std::string tok;
+		std::stringstream ss(ln);
+		while(ss >> tok)
+			toks.push_back(tok);
+
+		for(unsigned i=0; i<toks.size(); i++) {
+			if(toks[i] == "\t" || toks[i] == "")
+				toks.erase(toks.begin()+i);
+			else if(toks[i][0] == ';')
+				toks.resize(i);
+		}
+		if(!toks.empty())
+			tokens.push_back(toks);
+	}
+	// DEBUG: print out what has been stored
+#ifdef _DEBUG
+	for(unsigned i=0; i<tokens.size(); i++) {
+		for(unsigned j=0; j<tokens[i].size(); j++) {
+			std::cout << tokens[i][j] << "_";
+		}
+		std::cout << std::endl;
+	}
+#endif
+}
+
+void Assembler::outputFile() {
+
 }
 
 void Assembler::useZeroFill() {
@@ -58,18 +101,50 @@ void Assembler::putMmap() {
 	writeMmap = true;
 }
 
+bool Assembler::importBin(const std::string& fn, int offs, int n, const std::string& label) {
+	return false;
+}
+
 void Assembler::writeOp(std::vector<std::string>& tokens) {
 	
 }
 
 inline void Assembler::op_void(OPCODE op) {
-	output.put(0x0);
-	output.put(0x0);
-	output.put(0x0);
+	
 }
 
 inline void Assembler::op_imm(OPCODE op,u16 imm) {
-	output.put(0x0);
-	output.put(imm & 0x00FF);
-	output.put(imm >> 8);
+	
+}
+
+inline void Assembler::op_n(OPCODE op,u8 n) {
+
+}
+
+inline void Assembler::op_n_n(OPCODE op,u8 n1,u8 n2) {
+
+}
+
+inline void Assembler::op_r_imm(OPCODE op,u8 r,u16 imm) {
+
+}
+
+inline void Assembler::op_r_n(OPCODE op,u8 r,u8 n) {
+
+}
+
+inline void Assembler::op_r_r_imm(OPCODE op, u8 r1,u8 r2,u16 imm) {
+
+}
+
+inline void Assembler::op_r_r_r(OPCODE op, u8 r1,u8 r2,u8 r3) {
+
+}
+
+void Assembler::db(std::vector<u8>& bytes) {
+
+}
+
+void Assembler::db(std::string& str) {
+
 }
